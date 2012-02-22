e618bd225e6bdb51004078b8228a88750c514e86
ben.khoo@gaiaresources.com.au <ben.khoo@gaiaresources.com.au@a1be47e5-e1a4-4eca-8b5b-22a79e575783>
2012-02-22 02:09:13 +0000

    Fixed database issue with User.roles being a copy of an array and having persistence issues.

--- trunk/src/main/java/au/com/gaiaresources/bdrs/model/user/User.java
+++ trunk/src/main/java/au/com/gaiaresources/bdrs/model/user/User.java
@@ -1,6 +1,5 @@
 package au.com.gaiaresources.bdrs.model.user;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -181,11 +180,13 @@ public class User extends PortalPersistentImpl implements Comparable<User> {
     @IndexColumn(name = "ROLE_ORDER")
     @Fetch(FetchMode.SUBSELECT)
     public String[] getRoles() {
-        return Arrays.copyOf(this.roles, this.roles.length);
+        //return Arrays.copyOf(this.roles, this.roles.length);
+        return this.roles;
     }
 
     public void setRoles(String[] roles) {
-        this.roles = Arrays.copyOf(roles, roles.length);
+        //this.roles = Arrays.copyOf(roles, roles.length);
+        this.roles = roles;
     }
 
     // Many to many is a work around (read hack) to prevent a unique
